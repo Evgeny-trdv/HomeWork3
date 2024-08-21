c41d2f6be1ebb7dfce7c0d0090624f7686e6fa54
evgen <evgeny15q@gmail.com>
2024-08-21 21:08:54 +0700

    First task of homework3

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        byte firstNumbers = 3;
+        short secondNumbers = 200;
+        int thirdNumbers = 5000;
+        long fourthNumbers = 12231499999L;
+        float fifthNumbers = 2.2f;
+        double sixthNumbers = 666.666;
+        System.out.println("Значение переменной firstNumbers с типом byte = " + firstNumbers);
+        System.out.println("Значение переменной secondNumbers с типом short = " + secondNumbers);
+        System.out.println("Значение переменной thirdNumbers с типом int = " + thirdNumbers);
+        System.out.println("Значение переменной fourthNumbers с типом long = " + fourthNumbers);
+        System.out.println("Значение переменной fifthNumbers с типом float = " + fifthNumbers);
+        System.out.println("Значение переменной sixthNumbers с типом double = " + sixthNumbers);
+    }
+}
